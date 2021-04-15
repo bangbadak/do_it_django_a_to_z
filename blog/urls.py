7db8ab904bccfd5f1dd73e63b6dec88fb3566ce2
Yoseph Lee <yosephlee050523@gmail.com>
2021-04-15 14:23:07 +0900

    태그 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('category/<str:slug>/', views.category_page),
+    path('tag/<str:slug>/', views.tag_page),
     path('<int:pk>/', views.single_post_page),
     path('', views.index),
     #path('', views.index),
