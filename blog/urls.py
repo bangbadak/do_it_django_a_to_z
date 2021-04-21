f19dd30a67337f4f8c70c91153af6f7e7bd28f42
Yoseph Lee <yosephlee050523@gmail.com>
2021-04-22 02:44:43 +0900

    포스트 수정 기능 추가하기

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('update_post/<int:pk>?', views.PostUpdate.as_view),
     path('create_psot/', views.PostCreate.as_view()),
     path('category/<str:slug>/', views.category),
     path('tag/<str:slug>/', views.tag_page),
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Post, Category, Tag
 from django.shortcuts import render, redirect
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.core.exceptions import PermissionDenied
 
 def PostList(ListView):
     model = Post
@@ -90,3 +91,13 @@ class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
             return redirect('/blog/')
 
 # Create your views here.
+class PostUpdate(LoginRequiredMixin, UpdateView):
+    model = Postfields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category', 'tags']
+
+    template_name = 'blog/post_update_form.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user == self.get_object().author:
+            return super(PostUpdate, self).dispatch(request, *args, **kwargs)
+        else:
+            raise PermissionDenied
