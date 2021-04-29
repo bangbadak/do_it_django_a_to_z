e34d8f3e0d42e7c24c2d7e7c8f892eaf7bf20717
Yoseph Lee <yosephlee050523@gmail.com>
2021-04-29 18:27:10 +0900

    검색 기능 구현하기

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('search/<str:q>/', views.PostSearch.as_view()),
     path('delete_comment/<int:pk>/', views.delete_comment),
     path('update_post/<int:pk>?', views.PostUpdate.as_view),
     path('create_psot/', views.PostCreate.as_view()),
