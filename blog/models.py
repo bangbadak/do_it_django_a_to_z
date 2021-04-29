25ac98b7073d7a4861c4c73b7c53abead41ebb32
Yoseph Lee <yosephlee050523@gmail.com>
2021-04-29 18:37:21 +0900

    사용자 아바타 보여주기

--- blog/models.py
+++ blog/models.py
@@ -51,8 +51,15 @@ class Comment(models.Model):
 
     def __str__(self):
         return f'{self.author}::{self.content}'
+
     def get_absolute_url(self):
         return f'{self.post.get_absolute_url()}#comment-{self.pk}'
+
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return 'f'https://doitdjango.com/avatar/id/부여받은 id/ 부여받은 key/svg/{self.author.email}'
 
 # Create your models here.
 
