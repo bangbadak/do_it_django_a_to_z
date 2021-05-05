4e05851de86931c2433a12487032329575614999
Yoseph Lee <yosephlee050523@gmail.com>
2021-05-05 12:58:38 +0900

    대문 페이지 수정하기

--- blog/models.py
+++ blog/models.py
@@ -59,7 +59,8 @@ class Comment(models.Model):
         if self.author.socialaccount_set.exists():
             return self.author.socialaccount_set.first().get_avatar_url()
         else:
-            return 'f'https://doitdjango.com/avatar/id/부여받은 id/ 부여받은 key/svg/{self.author.email}'
+            return f'https://doitdjango.com/avatar/id/부여받은 id/ 부여받은 key/svg/{self.author.email}'
+
 
 # Create your models here.
 
